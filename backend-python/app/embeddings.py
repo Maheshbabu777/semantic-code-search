f570986839205012317233cc75910d6042e0a6cc
mahesh <maheshbabuvishnumolakala@gmail.com>
2026-04-02 23:04:54 +0530

    feat: add HF token not to get the warnings

--- backend-python/app/embeddings.py
+++ backend-python/app/embeddings.py
@@ -1,9 +1,14 @@
+import os
 import numpy as np
+from dotenv import load_dotenv
 from sentence_transformers import SentenceTransformer
 
+load_dotenv()
+
+HF_TOKEN = os.getenv("HF_TOKEN")
 MODEL_NAME = "flax-sentence-embeddings/st-codesearch-distilroberta-base"
 
-model = SentenceTransformer(MODEL_NAME)
+model = SentenceTransformer(MODEL_NAME, token=HF_TOKEN)
 
 def get_embedding(text: str) -> list[float]:
     embedding = model.encode(text, normalize_embeddings=True)
